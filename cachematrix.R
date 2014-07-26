52974168887cfb3094d85551dc1dbe83e9c601de
happypig <jeffwu411@gmail.com>
2014-07-26 16:45:57 +0800

    Revert "assignment is done"
    This reverts commit 50f603ab0860b614d3762a2f9d295c9a02275b93.

--- cachematrix.R
+++ cachematrix.R
@@ -1,37 +1,15 @@
-## Cache the inverse of a matrix
+## Put comments here that give an overall description of what your
+## functions do
 
-## This function creates a special "matrix" object that can cache its
-## inverse
+## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {  ##
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x
-  setinverse <- function(solve) m <<- solve  ## 
-  getinverse <- function() m                 ##
-  list(set = set, get = get,
-       setinverse = setinverse,              ##
-       getinverse = getinverse)              ##
-  
+makeCacheMatrix <- function(x = matrix()) {
+
 }
 
 
-## This function computes the inverse of the special "matrix" returned
-## by makeCacheMatrix above. If the inverse has already been calculated
-## (and the matrix has not changed), then the cachesolve should retrieve
-## the inverse from the cache
+## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {              ##
-  m <- x$getinverse()                         ##
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)                       ##
-  x$setinverse(m)                             ##
-  m
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
 }
